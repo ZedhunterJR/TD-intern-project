92f9b73adebc5314dfd88b26f81224b3345b02b4
ZedhunterJR <89736834+ZedhunterJR@users.noreply.github.com>
2025-03-18 22:46:24 +0700

    Add files via upload

---
+++ Assets/Script/GameManager/GameManager.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] TileManager tileManager;
+    [SerializeField] EnemyManager enemyManager;
+    [SerializeField] TowerManager towerManager;
+    [SerializeField] PoolManager poolManager;
+    [SerializeField] UIManager uiManager;
+    [SerializeField] WaveManager waveManager;
+    [SerializeField] PathManager pathManager;
+
+    // Win/Lose Condition 
+    [Header("Win / Lose Condition")]
+    [SerializeField] float baseHealth = 3;
+    private float currentHealth;
+    [SerializeField] Image healthBar;
+
+    // Game Status
+    private GAME_STATUS status;
+
+    private void Awake()
+    {
+        if (status == GAME_STATUS.Init)
+        {
+            currentHealth = baseHealth;
+            status = GAME_STATUS.Init;
+            Time.timeScale = 1;
+
+            //if (pathManager != null)
+            //    pathManager.OnAwake();
+        }
+    }
+
+    private void Start()
+    {
+        ChangeStatus(GAME_STATUS.Playing);
+    }
+
+    private void Update()
+    {
+        if (status == GAME_STATUS.Playing)
+        {
+            
+        }
+    }
+
+    #region Condition Win Lose and Change Game Status
+    public void TakeDame()
+    {
+        currentHealth -= 1;
+        currentHealth = Mathf.Clamp(currentHealth, 0, baseHealth);
+        UpdateHealthBar();
+        Debug.Log(currentHealth);
+        if (currentHealth == 0)
+        {
+            ChangeStatus(GAME_STATUS.Lose);
+        }
+    }
+
+    public void ChangeStatus(GAME_STATUS newStatus)
+    {
+        Debug.Log($"Current status: {status}");
+        if (status != newStatus)
+        {
+            status = newStatus;
+            switch (status)
+            {
+                case GAME_STATUS.Init:
+                    Debug.Log($"Change status done!! {status}");
+                    break;
+                case GAME_STATUS.Playing:
+                    Debug.Log($"Change status done!! {status}");
+                    PauseGame(2);
+                    break;
+                case GAME_STATUS.Pause:
+                    Debug.Log($"Change status done!! {status}");
+                    PauseGame(1);
+                    break;
+                case GAME_STATUS.Win:
+                    Debug.Log($"Change status done!! {status}");
+                    PauseGame(1);
+                    break;
+                case GAME_STATUS.Lose:
+                    Debug.Log($"Change status done!! {status}");
+                    uiManager.ActivePanel(GAME_STATUS.Lose);
+                    PauseGame(1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = currentHealth / baseHealth;
+    }
+    #endregion
+
+    private bool gameIsPause = false;
+    /// <summary>
+    /// 0 = invert, 1 = pause, 2 = unpause
+    /// </summary>
+    /// <param name="pauseStatus"></param>
+    public void PauseGame(int pauseStatus)
+    {
+        switch (pauseStatus)
+        {
+            case 0: gameIsPause = !gameIsPause; break;
+            case 1: gameIsPause = true; break;
+            case 2: gameIsPause = false; break;
+        }
+        if (gameIsPause)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+    #region Load Random Map
+    void LoadRandomMap()
+    {
+        GameObject[] maps = Resources.LoadAll<GameObject>("Map");
+
+        if (maps.Length == 0)
+        {
+            Debug.Log("Không tìm thấy map nào");
+            return;
+        }
+
+        int randomIndex;
+        do
+        {
+            randomIndex = Random.Range(0, maps.Length);
+        }
+        while (randomIndex == PlayerPrefs.GetInt("LastMapIndex", -1)); // Kiểm tra map có trùng lần trước không
+
+        // Lưu lại map đã chơi để tránh trùng
+        PlayerPrefs.SetInt("LastMapIndex", randomIndex);
+        PlayerPrefs.Save();
+
+        GameObject selectedMap = maps[randomIndex];
+
+        // Instantiate map vào game
+        Instantiate(selectedMap, Vector3.zero, Quaternion.identity);
+    }
+    #endregion
+}
+
+public enum GAME_STATUS
+{
+    Init,
+    Playing,
+    Pause,
+    Win,
+    Lose
+}
---
+++ Assets/Script/GameManager/TileManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileManager : Singleton<TileManager>
+{
+    [SerializeField]
+    List<TileEntity> tiles = new List<TileEntity>();
+
+    [SerializeField]
+    List<TowerData> listData = new List<TowerData>();
+
+    //[SerializeField] Button spawnTower;
+
+    private void Start()
+    {
+        InitAllTiles();
+        /* Already linked this from CanvasAction/Content1/GameObject/Button
+        spawnTower.onClick.AddListener(SpawnRandomTile);*/
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) 
+            SpawnRandomTile();
+    }
+
+    void InitAllTiles()
+    {
+        tiles = new();
+        tiles.AddRange(FindObjectsOfType<TileEntity>());
+        foreach (var tile in tiles)
+        {
+            tile.Init();
+        }
+    }
+
+    public void SpawnRandomTile()
+    {
+        List<TileEntity> tilesNoneTower = tiles.Where(n => n.Status == TILE_BUILDING_STATUS.None).ToList();
+
+        if (tilesNoneTower.Count > 0)
+        {
+            TileEntity tile = tilesNoneTower.GetRandom();
+            tile.ChangeStatus(TILE_BUILDING_STATUS.HasTower);
+            //TestEnemyAndTowerSpawn.Instance.SpawnTower(tile.transform.position);
+            GameObject tower = PoolManager.Instance.GetTowerFromPool();
+            tower.GetComponent<TowerStat>().Init(listData.GetRandom());
+            tower.transform.position = Vector2.zero;
+            tower.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Không còn chỗ trống để spawn");
+        }
+    }
+}
