21dd5da92e0ad9d895302be0ac34f26e497e0e94
vumanhtoan08 <aypro2003@gmail.com>
2025-03-24 02:57:02 +0700

    Complete logic and handle UI menu

--- Assets/Script/MenuManger/ShopManager.cs
+++ Assets/Script/MenuManger/ShopManager.cs
@@ -17,14 +17,22 @@ public class ShopManager : Singleton<ShopManager>
     [SerializeField] private TextMeshProUGUI upgradeAttackCostText;
     [SerializeField] private TextMeshProUGUI upgradeAttackSpeedCostText;
 
+    [SerializeField] private TextMeshProUGUI levelTileText;
+    [SerializeField] private TextMeshProUGUI levelHeartText;
+    [SerializeField] private TextMeshProUGUI levelAttackText;
+    [SerializeField] private TextMeshProUGUI levelAttackSpeedText;
+
+    [SerializeField] private TextMeshProUGUI detailTileText;
+    [SerializeField] private TextMeshProUGUI detailHeartText;
+    [SerializeField] private TextMeshProUGUI detailAttackText;
+    [SerializeField] private TextMeshProUGUI detailAttackSpeedText;
+
     public void OnAwake()
     {
         UpdateUpgradeCostTileUI();
         UpdateUpgradeCostHeartUI();
         UpdateUpgradeCostAttackUI();
         UpdateUpgradeCostAttackSpeedUI();
-
-    
     }
 
     private void Start()
@@ -42,10 +50,14 @@ public class ShopManager : Singleton<ShopManager>
         if (DataManager.Instance.PlayerData.tileLevel >= 5)
         {
             upgradeTileCostText.text = $"Max Lv";
+            levelTileText.text = $"Level {DataManager.Instance.PlayerData.tileLevel}";
+            detailTileText.text = $"Current Max Tile Building: {DataManager.Instance.PlayerData.maxTileBuilding}";
             return;
         }
 
         upgradeTileCostText.text = $"{cost}G";
+        levelTileText.text = $"Level {DataManager.Instance.PlayerData.tileLevel}";
+        detailTileText.text = $"Max Tile Building: {DataManager.Instance.PlayerData.maxTileBuilding}";
     }
 
     public void UpdateUpgradeCostHeartUI()
@@ -55,10 +67,14 @@ public class ShopManager : Singleton<ShopManager>
         if (DataManager.Instance.PlayerData.heartLevel >= 3)
         {
             upgradeHeartCostText.text = $"Max Lv";
+            levelHeartText.text = $"Level {DataManager.Instance.PlayerData.heartLevel}";
+            detailHeartText.text = $"Max Heart: {DataManager.Instance.PlayerData.maxHeart}";
             return;
         }
 
         upgradeHeartCostText.text = $"{cost}G";
+        levelHeartText.text = $"Level {DataManager.Instance.PlayerData.heartLevel}";
+        detailHeartText.text = $"Max Heart: {DataManager.Instance.PlayerData.maxHeart}";
     }
 
     public void UpdateUpgradeCostAttackUI()
@@ -68,10 +84,14 @@ public class ShopManager : Singleton<ShopManager>
         if (DataManager.Instance.PlayerData.bonusAttackLevel >= 5)
         {
             upgradeAttackCostText.text = $"Max Lv";
+            levelAttackText.text = $"Level {DataManager.Instance.PlayerData.bonusAttackLevel}";
+            detailAttackText.text = $"Attack Bonus: {DataManager.Instance.PlayerData.bonusAttack}%";
             return;
         }
 
         upgradeAttackCostText.text = $"{cost}G";
+        levelAttackText.text = $"Level {DataManager.Instance.PlayerData.bonusAttackLevel}";
+        detailAttackText.text = $"Attack Bonus: {DataManager.Instance.PlayerData.bonusAttack}%";
     }
 
     public void UpdateUpgradeCostAttackSpeedUI()
@@ -81,10 +101,14 @@ public class ShopManager : Singleton<ShopManager>
         if (DataManager.Instance.PlayerData.bonusAttackSpeedLevel >= 5)
         {
             upgradeAttackSpeedCostText.text = $"Max Lv";
+            levelAttackSpeedText.text = $"Level {DataManager.Instance.PlayerData.bonusAttackSpeedLevel}";
+            detailAttackSpeedText.text = $"Attack Speed Bonus: {DataManager.Instance.PlayerData.bonusAttackSpeed}%";
             return;
         }
 
         upgradeAttackSpeedCostText.text = $"{cost}G";
+        levelAttackSpeedText.text = $"Level {DataManager.Instance.PlayerData.bonusAttackSpeedLevel}";
+        detailAttackSpeedText.text = $"Attack Speed Bonus: {DataManager.Instance.PlayerData.bonusAttackSpeed}%";
     }
     #endregion
 }
